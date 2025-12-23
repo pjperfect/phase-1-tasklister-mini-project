c56757b826932fc27f0eb912f40987ffc76532a1
Philip Olembo <pjole.kenya@gmail.com>
2025-12-23 17:20:39 +0300

    Lab: HTML Forms- Tasklister Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskInput = event.target.elements['new-task-description'];
+    const task = taskInput.value;
+    buildToDo(task);
+    taskInput.value = '';
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement('li');
+    li.innerHTML = task;
+    taskList.appendChild(li);
+  }
 });
